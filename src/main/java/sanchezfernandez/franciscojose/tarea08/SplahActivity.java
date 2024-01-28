87763c0e69b57875ceb5a435b2c68a939e82d834
FranS <franjs.dam@gmail.com>
2024-01-28 18:02:07 +0100

    creación de SplashActivity

---
+++ src/main/java/sanchezfernandez/franciscojose/tarea08/SplahActivity.java
@@ -0,0 +1,14 @@
+package sanchezfernandez.franciscojose.tarea08;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah);
+    }
+}
